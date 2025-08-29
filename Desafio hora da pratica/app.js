ee8af73ea23ce74a5134ca54f0cf4dac9aaecf87
Erick Nascimento <erickrochaluciano@gmail.com>
2025-08-29 14:16:08 +0000

    Upload atividades

---
+++ Desafio hora da pratica/app.js
@@ -0,0 +1,14 @@
+alert('Boas vindas ao nosso site!');
+let nome = 'Lua';
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+
+
+alert(mensagemDeErro);
+
+let idade = prompt('Digite sua idade:');
+if (idade >= 18) {
+    alert('Pode tirar a habilitação!');
+}else alert('Não pode a tirar habilitação!');
+
