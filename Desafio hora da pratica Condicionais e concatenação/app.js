afc5cf6168732f2611baede28506af2eeb71dfd8
Erick Nascimento <erickrochaluciano@gmail.com>
2025-08-30 19:57:06 +0000

    Atualização Desafio

--- Desafio hora da pratica Condicionais e concatenação/app.js
+++ Desafio hora da pratica Condicionais e concatenação/app.js
@@ -1,14 +1,7 @@
 alert('Boas vindas ao nosso site!');
-let nome = 'Lua';
-let numeroDeVendas = 50;
-let saldoDisponivel = 1000;
-let mensagemDeErro = 'Erro! Preencha todos os campos';
 
+let diaDaSemana = prompt('Qual dia da semana é hoje?');
 
-alert(mensagemDeErro);
-
-let idade = prompt('Digite sua idade:');
-if (idade >= 18) {
-    alert('Pode tirar a habilitação!');
-}else alert('Não pode a tirar habilitação!');
-
+if(diaDaSemana == 'Sábado', diaDaSemana == 'Domingo'){
+    alert('Bom fim de semana!');
+}else alert('Boa semana!');
