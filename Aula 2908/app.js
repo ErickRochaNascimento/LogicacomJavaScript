c39e31a6b9493a2637dce3fc7a98c4213cd5e35a
Erick Nascimento <erickrochaluciano@gmail.com>
2025-08-30 19:47:20 +0000

    Atualização App.js

--- Aula 2908/app.js
+++ Aula 2908/app.js
@@ -4,5 +4,5 @@ console.log(numeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 30');
 
 if ( chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}!`);
+    alert(`]Isso ai! Você descobriu o número secreto ${numeroSecreto}!`);
 }else alert(`Você errou :( O numero secreto é ${numeroSecreto}`);
