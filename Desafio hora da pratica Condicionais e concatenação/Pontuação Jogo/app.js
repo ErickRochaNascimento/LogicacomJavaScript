2676b02df9420019198b6fd361909d1efdcb0dcf
Erick Nascimento <erickrochaluciano@gmail.com>
2025-08-30 22:15:38 +0000

    Criação Pontuação Jogo

---
+++ Desafio hora da pratica Condicionais e concatenação/Pontuação Jogo/app.js
@@ -0,0 +1,7 @@
+alert('Boas vindas ao nosso site!');
+
+let numeroDePontos = prompt('Qual a sua pontuação?');
+
+if(numeroDePontos >= 100){
+    alert('Parabéns, você venceu!');
+}else alert('Tente novamente para ganhar.');
