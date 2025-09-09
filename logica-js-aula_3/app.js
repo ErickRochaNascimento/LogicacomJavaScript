dd72ea04b4bbfbcac8b587ca25a069f7228361a1
Erick Nascimento <erickrochaluciano@gmail.com>
2025-09-09 14:29:40 +0000

    a

--- logica-js-aula_3/app.js
+++ logica-js-aula_3/app.js
@@ -23,3 +23,6 @@ while (chute != numeroSecreto) {
 
 let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
 alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+
+
+let carro;
